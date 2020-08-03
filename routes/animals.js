4e964ee6fe25f843d27a675a397f73c28ad27d74
Weily <weily.lang@gmail.com>
2020-08-03 14:19:38 -0400

    cleaned up commented out code

--- routes/animals.js
+++ routes/animals.js
@@ -3,38 +3,23 @@ const router = express.Router();
 
 const  {
     getAllAnimals, 
-    // getAllOutfits,
     createAnimal, 
-    // createOutfit,
     updateAnimal,
-    // updateOutfit,
-    deleteAnimal//,
-    // deleteOutfit
+    deleteAnimal
 } = require('../controllers/animals');
 
 // get all Animals
 router.get("/", getAllAnimals)
 
-// get all Outfits 
-// router.get("/outfits", getAllOutfits)
-
 // add new Animal
 router.post("/", createAnimal)
-
-// // add new Outfit
-// router.post("/outfits", createOutfit)
 
 // update existing Animal
 router.put("/:id", updateAnimal)
 
-// // update existing Outfit
-// router.put("/outfits/:id", updateOutfit)
 
 // delete existing Animal
 router.delete("/:id", deleteAnimal)
-
-// // delete existing Outfit
-// router.delete("/outfits/:id", deleteOutfit)
 
 
 module.exports = router;
